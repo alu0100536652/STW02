decd2fccb315d9dfd8ee5489fe74e25f35776b87
David CR <alu0100536652@ull.edu.es>
2015-10-08 22:55:10 +0000

    Modificando Test

--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,29 @@
 var assert = chai.assert;
 
-suite('temperature', function() {
-    test('32F = 0C', function() {
+suite('Conversor de temperatura', function() {
+    test('0 Celsius deben ser 32 Farenheit', function() {
+        original.value = "0c";
+        calcular();
+        assert.equal(converted.innerHTML, "32.0 Farenheit");
+    });
+    test('32 Farenheit deben ser 0 Celsius', function() {
         original.value = "32f";
         calcular();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+        assert.equal(converted.innerHTML, "0.0 Celsius");
     });
-    test('45C = 113.0 Farenheit', function() {
+    test('45 Celsius no puede ser 0 Farenheit', function() {
         original.value = "45C";
         calcular();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+        assert.notEqual(converted.innerHTML, "0.0 Farenheit");
     });
-    test('5X = error', function() {
+    test('5A debe dar un Error!', function() {
         original.value = "5X";
         calcular();
         assert.deepEqual(converted.innerHTML, "Error! Revise los datos.");
+    });
+    test('s debe dar un  Error!', function() {
+        original.value = "s";
+        calcular();
+        assert.notDeepEqual(converted.innerHTML, "0.0 Farenheit");
     });
 });
